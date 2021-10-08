b9b897af32856a6a149a25f186519cb8924308a5
Maki Aboabida <maki-miko@hotmail.com>
2021-10-08 18:12:27 -0400

    Add: prompt user (#2)

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer')
-const db = require('./db/connection')
-const apiRoutes = require('./routes/apiRoutes')
+// const db = require('./db/connection')
+// const apiRoutes = require('./routes/apiRoutes')
 // EXPRESS
 const express = require('express')
 const app = express()
@@ -18,11 +18,32 @@ app.listen(PORT, () => {
     console.log(`Server rendering on port ${PORT}!`)
 })
 
+let promptUser = () => {
+    return inquirer.prompt([
+        {
+            name: 'mainList',
+            message: 'What would you like to do?',
+            type: 'list',
+            choices: [
+                "view all departments",
+                "view all roles",
+                "view all employees",
+                "add a department",
+                "add a role",
+                "add an employee",
+                "update an employee role"
+            ]
+        }
+    ])
+} 
+
+promptUser()
+
 // Start server after DB connection
-db.connect(err => {
-    if (err) throw err
-    console.log('Database connected.')
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`)
-    })
-  })
+// db.connect(err => {
+//     if (err) throw err
+//     console.log('Database connected.')
+//     app.listen(PORT, () => {
+//       console.log(`Server running on port ${PORT}`)
+//     })
+//   })
